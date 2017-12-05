a69f4ef5a333c41a18dd47c111436b67d113a7cb
wk-j <somnuk.wk@gmail.com>
2017-12-05 14:28:03 +0700

    Find test methods

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ## Test Filter
 
 - https://github.com/dotnet/cli/blob/master/src/dotnet/commands/dotnet-test/TestCommandParser.cs
+
+```
+dotnet msbuild src/DotNetFilter.IntegrateTests /t:VSTest
+dotnet msbuild src/DotNetFilter.IntegrateTests /t:VSTest /p:VSTestListTests=true
+dotnet msbuild src/DotNetFilter.IntegrateTests /t:VSTest /p:VSTestTestCaseFilter=DisplayName~test
+
+dotnet test src/DotNetFilter.IntegrateTests -t 
+dotnet test src/DotNetFilter.IntegrateTests --filter DisplayName~My
+```
--- src/DotNetFilter.IntegrateTests/Tests.fs
+++ src/DotNetFilter.IntegrateTests/Tests.fs
@@ -1,8 +1,28 @@
 module Tests
 
-open System
 open Xunit
+open System.Reflection
+
+[<Fact>]
+let shouldGetTests() = 
+    let dll = "/Users/wk/Source/DotNetFilter/src/DotNetFilter.Tests/bin/Debug/netcoreapp2.0/DotNetFilter.Tests.dll"
+    let asm = Assembly.LoadFile(dll)
+    let info = 
+        query {
+            for types in asm.GetTypes() do
+            for method in types.GetMethods() do
+            for att in method.GetCustomAttributes() do
+            where (att.GetType().Name = "FactAttribute")
+            select (types.FullName, method.Name)
+        }
+
+    for t in info do
+        t |> printfn "-- %A"
 
 [<Fact>]
 let ``My test`` () =
+    Assert.True(true)
+
+[<Fact>]
+let ``Your test``() = 
     Assert.True(true)
